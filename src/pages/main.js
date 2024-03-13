2c3e21ee97dae75a16fce260e025ce507e264c90
Shirley <xiaochunyan0218@126.com>
2024-03-13 18:49:14 +0800

    小尺寸适配优化

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,17 +2,15 @@ import './main.css'
 import TopBar from './topbar';
 import SideBar from './sidebar';
 import Content from './content';
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function Main() {
     const ref = useRef('')
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         scaleByScreenWidth()
-    //     })
-    // })
+    useEffect(() => {
+        window.addEventListener('resize', onResize)
+        return () => window.removeEventListener('resize', onResize)
+    })
     
-  
     return <div className="block" ref={ref}>
         <SideBar/>
         <div>
@@ -21,12 +19,10 @@ export default function Main() {
         </div>
     </div>
 
-    
-    // function scaleByScreenWidth() {
-    //     const width = window.outerWidth
-    //     if (ref.current) {
-    //         ref.current.style.scale = width < 500 ? width / 800 : width < 1000 ? 0.8 : 1
-    //         console.log( width / 500)
-    //     }
-    // }
+    function onResize() {
+        if (ref.current) {
+            const width = window.outerWidth
+            ref.current.style.scale = width < 460 ? (width / 600) : 1
+        }
+    }
 }
